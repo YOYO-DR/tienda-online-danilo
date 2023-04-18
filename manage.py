64570c9c33224fdf0083f7a9d94b69a07c251aa9
yoiner_duran <duranyoiner86@gmail.com>
2023-04-18 14:53:07 -0500

    actualización del profe

--- manage.py
+++ manage.py
@@ -2,12 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
+
 
 def main():
-    load_dotenv('./.env')
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
