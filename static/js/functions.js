3f5afc1381b7d57efc68d0d1122ae0a8eebe8ae2
yoiner_duran <duranyoiner86@gmail.com>
2023-05-02 15:44:35 -0500

    arreglando bug en la eliminación rapida de carrito

--- static/js/functions.js
+++ static/js/functions.js
@@ -21,7 +21,7 @@ function enviar_datos(url, datos, callback) {
         toast: true,
         position: "top-end",
         showConfirmButton: false,
-        timer: 1500,
+        timer: 4000,
         timerProgressBar: true,
         didOpen: (toast) => {
           toast.addEventListener("mouseenter", Swal.stopTimer);
@@ -35,19 +35,40 @@ function enviar_datos(url, datos, callback) {
       });
       //*************
     });
-};
+}
 
-function carritoAcciones(itemId,url,accion) {
-      let datos = {
-        pk: itemId,
-        action: accion,
-      };
-  enviar_datos(url, datos, function (data) {//funcion que realiza acciones despues de la petición
-      if (data["error"]) {
-        //****************** que hubo un error
+function carritoAcciones(itemId, url, accion) {
+  let datos = {
+    pk: itemId,
+    action: accion,
+  };
+  enviar_datos(url, datos, function (data) {
+    //funcion que realiza acciones despues de la petición
+    if (data["error"]) {
+      //****************** que hubo un error
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener("mouseenter", Swal.stopTimer);
+          toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+      });
+
+      Toast.fire({
+        icon: "error",
+        title: `${data["error"]}`,
+      });
+      //*************
+    } else {
+      if (accion == "add_cart") {
+        //****************** de que se agrego correctamente
         const Toast = Swal.mixin({
           toast: true,
-          position: "top-end",
+          position: "bottom-end",
           showConfirmButton: false,
           timer: 1500,
           timerProgressBar: true,
@@ -58,77 +79,58 @@ function carritoAcciones(itemId,url,accion) {
         });
 
         Toast.fire({
-          icon: "error",
-          title: `${data["error"]}`,
+          icon: "success",
+          title: `${data["mensaje"]}`,
         });
         //*************
-      } else {
-        if (accion == "add_cart") {
-          //****************** de que se agrego correctamente
-          const Toast = Swal.mixin({
-            toast: true,
-            position: "bottom-end",
-            showConfirmButton: false,
-            timer: 1500,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener("mouseenter", Swal.stopTimer);
-              toast.addEventListener("mouseleave", Swal.resumeTimer);
-            },
-          });
+        const pTotal = document.getElementById("cart-total"); //obtengo el p del carrito
+        pTotal.innerHTML = `${data["can_carrito"]}`; //le pongo la cantidad
 
-          Toast.fire({
-            icon: "success",
-            title: `${data["mensaje"]}`,
-          });
-          //*************
-          const pTotal = document.getElementById("cart-total"); //obtengo el p del carrito
-          pTotal.innerHTML = `${data["can_carrito"]}`; //le pongo la cantidad
-
-          //si es alguna de las 3 acciones del carrito lo pongo aqui
-        } else if (accion == "eliminar") {
-          var trElim = document.getElementById("item-" + itemId); //obtengo el tr del item y lo quito si la petición de eliminar es correcta
+        //si es alguna de las 3 acciones del carrito lo pongo aqui
+      } else if (accion == "eliminar") {
+        var trElim = document.getElementById("item-" + itemId); //obtengo el tr del item y lo quito si la petición de eliminar es correcta
+        if (trElim) {
+          //para que al eliminar rapido no se demore y salga un error
           trElim.remove();
-        } else if (accion == "aumentar" || accion == "disminuir") {
-          const cantidad = document.getElementById(`product/${itemId}`); //donde va la cantidad
-          const total = document.getElementById(`total_product/${itemId}`); //donde va el total
-          cantidad.innerHTML = `${data["can_cantidad"]}`;
-          totalN = data["can_total"].toLocaleString("es-ES", {
-            minimumFractionDigits: 1,
-            maximumFractionDigits: 1,
-          });
-          total.innerHTML = `${totalN}`;
-          //quitarle el disabled al boton
-          if (accion == "aumentar") {
-            const aumentar = document.getElementById(`${itemId}/aumentar`);
-            aumentar.removeAttribute('disabled');
-          } else {
-            const disminuir = document.getElementById(`${itemId}/disminuir`);
-            disminuir.removeAttribute("disabled");
-          }
         }
-        if (
-          accion == "aumentar" ||
-          accion == "disminuir" ||
-          accion == "eliminar"
-        ) {
-          //independientemente de la accion, eliminar, aumentar o disminuir, se modifica el total del checkout
-          const checkoutTotal = document.getElementById(`checkout_total`);
-          checkTotalN = data["check_total"].toLocaleString("es-ES", {
-            //para convertirlo a numero natural o el intcomma de django
-            minimumFractionDigits: 1,
-            maximumFractionDigits: 1,
-          });
-          checkoutTotal.innerHTML = `${checkTotalN}`;
-          if (data["check_total"] == 0) {
-            //si el el total del carrito es 0, remplazo todo el contenido y pongo un mensaje
-            const contenedorABorrar =
-              document.getElementById("contenedor_todo");
-            contenedorABorrar.innerHTML =
-              "<h2>No hay productos en el carrito</h2>";
-          }
+      } else if (accion == "aumentar" || accion == "disminuir") {
+        const cantidad = document.getElementById(`product/${itemId}`); //donde va la cantidad
+        const total = document.getElementById(`total_product/${itemId}`); //donde va el total
+        cantidad.innerHTML = `${data["can_cantidad"]}`;
+        totalN = data["can_total"].toLocaleString("es-ES", {
+          minimumFractionDigits: 1,
+          maximumFractionDigits: 1,
+        });
+        total.innerHTML = `${totalN}`;
+        //quitarle el disabled al boton
+        if (accion == "aumentar") {
+          const aumentar = document.getElementById(`${itemId}/aumentar`);
+          aumentar.removeAttribute("disabled");
+        } else {
+          const disminuir = document.getElementById(`${itemId}/disminuir`);
+          disminuir.removeAttribute("disabled");
         }
       }
+      if (
+        accion == "aumentar" ||
+        accion == "disminuir" ||
+        accion == "eliminar"
+      ) {
+        //independientemente de la accion, eliminar, aumentar o disminuir, se modifica el total del checkout
+        const checkoutTotal = document.getElementById(`checkout_total`);
+        checkTotalN = data["check_total"].toLocaleString("es-ES", {
+          //para convertirlo a numero natural o el intcomma de django
+          minimumFractionDigits: 1,
+          maximumFractionDigits: 1,
+        });
+        checkoutTotal.innerHTML = `${checkTotalN}`;
+        if (data["check_total"] == 0) {
+          //si el el total del carrito es 0, remplazo todo el contenido y pongo un mensaje
+          const contenedorABorrar = document.getElementById("contenedor_todo");
+          contenedorABorrar.innerHTML =
+            "<h2>No hay productos en el carrito</h2>";
+        }
+      }
+    }
   });
-  };
- 
+}
